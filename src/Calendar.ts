143da3f3640a0991998e3d7fd07523ef7847e8c7
Madan Sapkota <contact@madansapkota.com>
2022-09-28 21:02:34 +0545

    Refactor the code

--- src/Calendar.ts
+++ src/Calendar.ts
@@ -1,4 +1,4 @@
-import { differenceInCalendarDays, endOfDay, startOfDay } from 'date-fns';
+import { differenceInCalendarDays } from 'date-fns';
 
 import { Years } from './data/Years';
 
@@ -6,12 +6,12 @@ export default class Calendar {
   /**
    * The calendar support start date -> 1975-01-01 BS
    */
-  protected readonly startDate: Date = startOfDay(new Date('1918-04-13'));
+  protected readonly startDate: Date = new Date('1918-04-13');
 
   /**
    * The calendar support end date -> 2099-12-30 BS
    */
-  protected readonly endDate: Date = endOfDay(new Date('2043-04-13'));
+  protected readonly endDate: Date = new Date('2043-04-13');
 
   /**
    * Convert numbers from nepali to english
@@ -35,7 +35,7 @@ export default class Calendar {
     // init BS date object
     const bsDate = { year: 0, month: 0, date: 0 };
     // calculate days difference
-    const calendarDays = Math.abs(differenceInCalendarDays(new Date(givenDate), new Date('1918-04-13')));
+    const totalDays = Math.abs(differenceInCalendarDays(new Date(givenDate), this.startDate));
     // init the counter
     let counter = 0;
 
@@ -44,13 +44,13 @@ export default class Calendar {
       // init days in the year
       const daysInYear = Years[year][12];
 
-      if (counter + daysInYear > calendarDays) {
+      if (counter + daysInYear > totalDays) {
         // init months
         const months = Years[year].slice(0, 12);
         months.some((days, mi) => {
-          if (counter + days > calendarDays) {
+          if (counter + days > totalDays) {
             // set output date
-            [bsDate.year, bsDate.month, bsDate.date] = [year, mi + 1, calendarDays - counter + 1];
+            [bsDate.year, bsDate.month, bsDate.date] = [year, mi + 1, totalDays - counter + 1];
             // exit from month loop
             return true;
           }
