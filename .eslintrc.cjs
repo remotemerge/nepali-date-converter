e84cac6ea67fdafb9d5588e1fc3083a98697c906
Madan Sapkota <contact@madansapkota.com>
2022-10-01 10:12:32 +0545

    Rollback eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,18 +5,11 @@ module.exports = {
     es2022: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:@typescript-eslint/strict',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
-    project: ['./tsconfig.json'],
     sourceType: 'module',
-    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
   rules: {
