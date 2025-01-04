75397c976c43ad30eb532a4cbbdd0f507e2f0172
Madan Sapkota <contact@madansapkota.com>
2025-01-04 04:19:05 +0000

    Improve the test cases

--- __tests__/ad.spec.ts
+++ __tests__/ad.spec.ts
@@ -1,22 +1,17 @@
 import DateConverter from '../src/index';
 
-import { default as records } from './data.json';
+import records from './data.json';
 
-// prefix zero
-const padZero = (num: number) => (num < 10 ? `0${num}` : num);
+const padZero = (num: number): string =>
+  num < 10 ? `0${num}` : num.toString();
 
-// test dates
-for (let i = 0; i < records.length; i++) {
-  const nepDate = records[i].nep;
-  const engDate = records[i].eng;
-
-  const { year, month, date } = new DateConverter(nepDate).toAd();
-  const resDate = `${year}-${padZero(month)}-${padZero(date)}`;
-
-  test(
-    'The Nepali date ' + nepDate + ' is equals to English date ' + resDate,
-    () => {
-      expect(resDate).toBe(engDate);
+describe('DateConverter Nepali to English Conversion', () => {
+  it.each(records)(
+    'converts Nepali date %s to English date %s',
+    ({ nep: nepaliDate, eng: expectedEnglishDate }) => {
+      const { year, month, date } = new DateConverter(nepaliDate).toAd();
+      const convertedEnglishDate = `${year}-${padZero(month)}-${padZero(date)}`;
+      expect(convertedEnglishDate).toBe(expectedEnglishDate);
     },
   );
-}
+});
--- __tests__/bs.spec.ts
+++ __tests__/bs.spec.ts
@@ -1,22 +1,17 @@
 import DateConverter from '../src/index';
 
-import { default as records } from './data.json';
+import records from './data.json';
 
-// prefix zero
-const padZero = (num: number) => (num < 10 ? `0${num}` : num);
+const padZero = (num: number): string =>
+  num < 10 ? `0${num}` : num.toString();
 
-// test dates
-for (let i = 0; i < records.length; i++) {
-  const nepDate = records[i].nep;
-  const engDate = records[i].eng;
-
-  const { year, month, date } = new DateConverter(engDate).toBs();
-  const resDate = `${year}-${padZero(month)}-${padZero(date)}`;
-
-  test(
-    'The English date ' + engDate + ' is equals to Nepali date ' + resDate,
-    () => {
-      expect(resDate).toBe(nepDate);
+describe('DateConverter', () => {
+  it.each(records)(
+    'converts English date %s to Nepali date %s',
+    ({ eng: englishDate, nep: expectedNepaliDate }) => {
+      const { year, month, date } = new DateConverter(englishDate).toBs();
+      const convertedNepaliDate = `${year}-${padZero(month)}-${padZero(date)}`;
+      expect(convertedNepaliDate).toBe(expectedNepaliDate);
     },
   );
-}
+});
