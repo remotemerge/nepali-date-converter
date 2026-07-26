1da104510f2ac6874328d44f43be25cec07a4ef1
Madan Sapkota <contact@madansapkota.com>
2026-07-26 14:44:52 +0545

    Update oxlint and oxfmt config

--- html/oxfmt.config.ts
+++ html/oxfmt.config.ts
@@ -2,4 +2,14 @@ import { defineConfig } from 'oxfmt';
 
 export default defineConfig({
   singleQuote: true,
+  printWidth: 80,
+  ignorePatterns: ['dist/**', 'output/**', 'public/**', 'src/*.js'],
+  overrides: [
+    {
+      files: ['**/*.scss', '**/*.css', '**/*.html', '**/*.yml'],
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 });
